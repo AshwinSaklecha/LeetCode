e2ff0921339f4b3d5c15dd1f599e234bb661cd24
Ashwin Saklecha <147031517+AshwinSaklecha@users.noreply.github.com>
2026-07-04 18:02:57 +0530

    Time: 0 ms (100%), Space: 19.3 MB (38.37%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.py
@@ -5,21 +5,17 @@ class Solution:
         visited = set()
         for word in bank:
             my_set.add(word)
-        ans = self.traverse(startGene, endGene, my_set, custom_words, visited)
-        return -1 if ans == float('inf') else ans
-    def traverse(self, curr_word, final_word, my_set, custom_words, visited):
-        if curr_word == final_word:
-            return 0
-        steps = float('inf')
-        for i in range(len(curr_word)):
-            for j in range(len(custom_words)):
-                each_step = float('inf')
-                if custom_words[j] == curr_word[i]:
-                    continue
-                new_word = curr_word[:i] + custom_words[j] + curr_word[i+1:]
-                if new_word in my_set and new_word not in visited:
-                    visited.add(new_word)
-                    each_step = 1 + self.traverse(new_word, final_word, my_set, custom_words, visited)
-                    visited.remove(new_word)
-                steps = min(steps, each_step)
-        return steps
+        qu = deque()
+        qu.append((startGene, 0))
+        visited.add(startGene)
+        while qu:
+            word, level = qu.popleft()
+            if word == endGene:
+                return level
+            for i in range(len(custom_words)):
+                for j in range(len(word)):
+                    new_word = word[:j] + custom_words[i] + word[j+1:]
+                    if new_word not in visited and new_word in my_set:
+                        visited.add(new_word)
+                        qu.append((new_word, level + 1))
+        return -1
